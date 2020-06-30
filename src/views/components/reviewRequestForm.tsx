48eeed1e0147a9555369ae3fd63ba6c4e545e330
t2015014 <abe_hi@tdc.co.jp>
2020-06-30 19:21:37 +0900

    レビュー作成フォーム項目追加

--- src/views/components/reviewRequestForm.tsx
+++ src/views/components/reviewRequestForm.tsx
@@ -4,7 +4,10 @@ import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import InputLabel from '@material-ui/core/InputLabel';
 import TextField from "@material-ui/core/TextField";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles((theme) => ({
@@ -40,6 +43,32 @@ export default function reviewRequestForm() {
                     <Typography className={classes.typography}>
                         レビュー作成フォーム
                     </Typography>
+                </CardContent>
+                <CardContent className={classes.cardContent}>
+                    <TextField className={classes.textField} label={"タイトル"} />
+                </CardContent>
+                <CardContent className={classes.cardContent}>
+                    <TextField className={classes.textField} label={"ドキュメントURL"} />
+                </CardContent>
+                <CardContent className={classes.cardContent}>
+                    <TextField className={classes.textField} label={"タグ"} />
+                </CardContent>
+                <CardContent className={classes.cardContent}>
+                    <TextField className={classes.textField} label={"参加者"} />
+                </CardContent>
+                <CardContent className={classes.cardContent}>
+                    <TextField className={classes.textField} label={"チェックリスト"} />
+                </CardContent>
+                <CardContent className={classes.cardContent}>
+                    <InputLabel id="review-request-scope-label">公開範囲</InputLabel>
+                    <Select className={classes.textField}
+                        labelId="review-request-scope-label"
+                        value={""}
+                    >
+                        <MenuItem value={"PUBLIC"}>公開</MenuItem>
+                        <MenuItem value={"PRIVATE"}>非公開</MenuItem>
+                        <MenuItem value={"PROJECT"}>プロジェクトメンバのみ</MenuItem>
+                    </Select>
                 </CardContent>
             </Card>
         </Container>
